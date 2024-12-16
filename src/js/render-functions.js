b01e441c9d7a7535d3119cf42f1be8554402dec1
GrigoriyK505 <167803933+GrigoriyK505@users.noreply.github.com>
2024-12-16 17:14:05 +0200

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -28,10 +28,12 @@ export function clearGallery() {
 }
 
 export function showLoader() {
+  const loader = document.querySelector(".loader");
   loader.style.display = "block";
 }
 
 export function hideLoader() {
+  const loader = document.querySelector(".loader");
   loader.style.display = "none";
 }
 
